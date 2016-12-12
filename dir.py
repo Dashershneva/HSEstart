df25957d059682aa89bd6764e65d2f708735faa2
Dashershneva <d.shershneva@gmail.com>
2016-12-12 20:42:32 +0300

    Add files via upload

---
+++ dir.py
@@ -0,0 +1,31 @@
+import os
+
+def func(c):
+    c = open('georgianalph.txt','r',encoding='utf-8')
+    b = c.read()
+    c.close()
+    m = b.split(' ')
+    c = open('ipa.txt','r',encoding='utf-8')
+    a = c.read()
+    c.close()
+    n = a.split(' ')
+    d{}
+    for letter in d:
+        if letter in a ==
+    
+    
+
+def func2(f):
+    f = open('georgianwiki.txt','r',encoding='utf-8')
+    s = f.read()
+    f.close()
+    for letter in s:
+        letter = letter.strip(' 1234567890;:.,—\ -|\\_\[\]\'\"{}!?')
+    s = list(s)
+    if letter in s == m[0]:
+        letter = d[0]
+        s = ' '.join(letter)
+    return s
+
+
+    
